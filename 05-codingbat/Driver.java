267694d7efc490b4f8bcefd6dbc971765bb1909a
Sally Bao <sallybao29@gmail.com>
2014-09-23 19:08:39 -0400

    Hw #5

---
+++ 05-codingbat/Driver.java
@@ -0,0 +1,8 @@
+public class Driver {
+    public static void main(String[]args) {
+	StringStuff s = new StringStuff();
+	System.out.println(s.nonStart("hello", "goodbye"));
+	System.out.println(s.makeAbba("yo", "lo"));
+	System.out.println(s.diff21(30));
+    }
+}
---
+++ 05-codingbat/StringStuff.java
@@ -0,0 +1,16 @@
+public class StringStuff {
+    public String nonStart(String a, String b) {
+	return a.substring(1) + b.substring(1);
+    }
+    public String makeAbba(String a, String b) {
+	return a + b + b + a;
+    }
+    public int diff21(int n) {
+	if (n > 21) {
+	    return 2 * (n - 21);
+	}
+	else {
+	    return 21 - n;
+	}
+    }
+}
