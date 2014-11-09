dd58b72a8f4f4a50d7685a09f6367491922ea10c
Sally Bao <sallybao29@gmail.com>
2014-11-08 23:24:55 -0500

    arrays

---
+++ 15-Sarray/Driver.java
@@ -0,0 +1,9 @@
+public class Driver{
+    public static void main(String[] args){
+	Sarray s = new Sarray();
+	s.add(5);
+	s.add(10);
+	s.remove(1);
+	System.out.println(s);
+    }
+}
--- 15-Sarray/Sarray.java
+++ 15-Sarray/Sarray.java
@@ -8,40 +8,94 @@ public class Sarray {
         // set up the initial instance variables
     }
 
+    public int[] grow(int[] nums){
+	int[] newArray = new int[nums.length + 1];
+	for (int i = 0;i < nums.length;i++){
+	    newArray[i] = nums[i];
+	}
+	return newArray;
+    }
+
+    public String toString(){
+	String s = "";
+	for (int i = 0;i < size();i++){
+	    s += data[i] + ", ";
+	}
+	return s;
+    }
+
     public boolean add(int i){
-	if (last == data.length - 1){
-	    int[] newArray = new int[data.length + 2];
-	    newArray[last + 1] = i;
-	    data = newArray;
-	}
-	else{
-	    data[last + 1] = i;
-	}
-	return true;
         // adds an item to the end of the list, grow if needed
         // returns true
+
+	int[] newArray;
+	if (last == data.length - 1){
+	    newArray = grow(data);
+	}
+	else{
+	    newArray = data;
+	}
+	newArray[last + 1] = i;
+	last++;
+	data = newArray;
+	return true;
     }
 
     public void  add(int index, int i){
         // adds item i  at index, shifting everything down as needed.
         // also grows as needed 
+
+	int[] newArray;
+	if (last == data.length - 1){
+	    newArray = grow(data);
+	}
+	else{
+	    newArray = data;
+	}
+	for (int n = last;n > index;n--){
+	    newArray[n] = newArray[n - 1];
+	}
+	last++;
+	newArray[index] = i;
+	data = newArray;
+
     }
 
     public int size() {
         // returns the number of items in the list (not the array size)
+	int count = 0;
+	for (int i = 0;i < last + 1;i++){
+	    count++;
+	}
+	return count;
     }
 
     public int get(int index) {
+
+	return data[index];
+
         // returns the item at location index of the lsit
     }
 
     public int set(int index, int i){
         // sets the item at location index to value i
         // returns the old value. 
+
+	int old = data[index];
+	data[index] = i;
+	return old;
+      
     }
 
     public int remove(int index){
         // removes the item at index i
         // returns the old value
+	int old = data[index];
+	for (int i = index;i < last;i++){
+	    data[i] = data[i + 1];
+	}
+	data[last] = 0;
+	last--;
+	return old;
     }
 }
