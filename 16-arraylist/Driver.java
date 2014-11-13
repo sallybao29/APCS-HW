3969435c964ed3fd45d56e795092a42233980ff6
Sally Bao <sallybao29@gmail.com>
2014-11-13 09:26:51 -0500

    m

--- 16-arraylist/Driver.java
+++ 16-arraylist/Driver.java
@@ -8,7 +8,7 @@ public class Driver{
 	for (int i = 0;i < 20;i++){
 	    ai.add(r.nextInt(5));
 	}
-
+	/*
 	System.out.println(ai);
      
 	for (int i = 0;i < ai.size() - 1;i++){
@@ -19,6 +19,27 @@ public class Driver{
 	}
 	         
 	System.out.println(ai);
+
+	*/
+
+	ArrayList<Integer> bob = new ArrayList<Integer>();
+	ArrayList<Integer> newBob = new ArrayList<Integer>();
+	Random randy = new Random();
+	for (int i = 0;i < 7;i++){
+	    bob.add(i);
+	}
+
+	for (int i = 0;i < 7;i++){
+	    newBob.add(0);
+	} 
+
+	System.out.println(bob);
+
+	for (int i = 0;i < 7;i++){
+	    int current = bob.get(i);
+	    newBob.add(r.nextInt(7), current);
+	}
+	System.out.println(newBob);
 
     }
     
