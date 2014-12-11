58df39ff6925d9e1912de6e280e69fb1c1a4b11d
Sally Bao <sallybao29@gmail.com>
2014-12-11 09:26:59 -0500

    a

--- 22-Searching/Searching.java
+++ 22-Searching/Searching.java
@@ -5,12 +5,19 @@ public class Searching{
 	a = new Comparable[numElements];
     }
 
-    public void additem(Comparable c){
-
+    public void additem(Comparable c, int i){
+	if (a[i] != null){
+	    a[i] = c;
+	}
     }
 
 
     public static void main(String[] args){
-
+	Searching s = new Searching(10);
+	
+	Random r = new Random();
+	for(int i = 0;i < s.length;i++){
+	    s.additem(r.nextInt(100));
+	}
     }
 }
